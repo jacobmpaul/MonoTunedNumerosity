e04ee550de93ee80170c8ee5ac70e02715b27e25
Jacob Paul <jpaacuolb@gmail.com>
2021-12-30 14:10:52 +1100

    Create Fig_1.m

---
+++ figures/Fig_1.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%
+%% Fig.1 %%
+%%%%%%%%%%%
+
+% Examples of the variables 'Constant area', 'Constant item size', 'Constant perimeter', and 'High density'
+% are shown in Fig.1
+load('StimulusImages.mat');
